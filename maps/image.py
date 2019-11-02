ae2e31702ac0043ff0b8b2b566311b575f9241ba
Marchetz92 <francescom394@gmail.com>
2019-11-02 21:45:20 +0100

    fix maps

--- maps/image.py
+++ maps/image.py
@@ -13,26 +13,26 @@ cm = LinearSegmentedColormap.from_list(
     cmap_name, colors, N=4)
 
 files = sorted(glob.glob('*.png'))
-pdb.set_trace()
 kernel = np.ones((5,5),np.uint8)
-pdb.set_trace()
 for f in files:
 
-    image = cv2.imread(f, 0)
-    image_copy = image.copy()
-    image_copy[np.where(image_copy == 2)] = 0
-    image_copy[np.where(image_copy == 3)] = 0
-    image_copy[np.where(image_copy == 4)] = 0
-    closing = cv2.morphologyEx(image_copy, cv2.MORPH_CLOSE, kernel)
-    image[np.where(closing == 1)] = 1
-    cv2.imwrite('closing/' + f[:-4] + '.png', image)
+    image = cv2.imread(f, 0) - 1
+    cv2.imwrite('nuove/' + f[:-4] + '.png', image)
+    # image_copy = image.copy()
+    # image_copy[np.where(image_copy == 2)] = 0
+    # image_copy[np.where(image_copy == 3)] = 0
+    # image_copy[np.where(image_copy == 4)] = 0
+    # closing = cv2.morphologyEx(image_copy, cv2.MORPH_CLOSE, kernel)
+    # image[np.where(closing == 1)] = 1
+    # #
+    #
+    # #to visualize with colors
+    # plt.imshow(image,cm)
+    # plt.savefig('png_closing/color/' + f[:-4] + '_5.png')
 
-    #to visualize with colors
-    #plt.imshow(image,cm)
-    #plt.savefig('png_closing/color/' + f[:-4] + '_5.png')
-
-
-
-
+    #
+    #
+    # plt.imshow(image,cm)
+    # plt.savefig('png/' + f[:-4] + '.png')
 
 
